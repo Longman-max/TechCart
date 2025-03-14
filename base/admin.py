49de4c9a9b996b718729bd321cf057a66217d339
Longman-max <147675147+Longman-max@users.noreply.github.com>
2025-03-14 12:30:41 +0100

    Update admin.py

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, User
 # Register your models here.
 # class ProductAdmin(admin.ModelAdmin):
 #     list_display = ('name', 'price', 'stock', 'description')  # Add the fields you want to display
-admin.site.register(Product)
+admin.site.register(Product, User)
